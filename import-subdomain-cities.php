8aeaede3b0c886fe64eb2f5443e82bd07157ee15
Sasha XUbuntu <third.pancake16@gmail.com>
2017-05-26 04:55:38 +0300

    Get factories

--- import-subdomain-cities.php
+++ import-subdomain-cities.php
@@ -2,4 +2,8 @@
 include 'bootstrap.php';
 
 $db = JFactory::getDBO();
-
+$dbNameFactories = $db->quoteName("#__multifactories_crudfactories");
+$getFactories_query = "SELECT * FROM $dbNameFactories";
+$db->setQuery($getFactories_query);
+$factories = $db->loadObjectList();
+var_dump($factories);
