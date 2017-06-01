a7fae11a4faa907d515e9daf6ec26a2578e4b789
Sasha XUbuntu <third.pancake16@gmail.com>
2017-06-01 18:06:48 +0300

    Removed unnecesary code in bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,5 @@ define( '_JEXEC', 1 );
 define( 'DS', DIRECTORY_SEPARATOR );
 define( 'JPATH_BASE', "/var/www/html/capmex.dev/public_html" ); 
 require_once JPATH_BASE . DS . 'includes'   . DS . 'defines.php';
-require_once JPATH_BASE . DS . 'libraries'  . DS . 'import.php';
-require_once JPATH_BASE . DS . 'configuration.php'; 
-require_once JPATH_BASE . '/includes/framework.php';
+require_once JPATH_BASE . DS . 'includes'   . DS . 'framework.php';
 // $mainframe =& JFactory::getApplication('site'); // optional. it will generate html, which is not great for console scripts
